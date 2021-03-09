2f96a9ff2c55693ca044aa8c348e442d89659055
CodeSEAS <manjun@seas.upenn.edu>
2021-03-09 11:55:25 -0500

    Initial Commit
    Some config is missing, will retrieve and fix. Land with the skeleton and gist of the project.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+import TopNavBar from './TopNavBar';
+import Main from './Main';
+
+function App() {
+    return (
+        <div className="App">
+            <TopNavBar />
+            <Main />
+        </div>
+    );
+
+}
+
+export default App;
